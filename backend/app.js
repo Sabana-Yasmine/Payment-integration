3bd96e70dbe864c28ae07cc94a9c37434af99d66
Sabana-Yasmine <sabana.platosys@gmail.com>
2022-09-10 14:07:00 +0530

    initial-commit

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+const express = require ("express")
+const braintree = require ("braintree")
+const cors = require  ("cors")
+const {response} = require ("express")
+const app = express();
+ 
+app.use(express.json());
+
+const config = {
+    envirionment : braintree.Environment.Sandbox,
+    merchantId : process.env.merchantId,
+    publicKey : process.env.publicKey,
+    privateKey : process.env.privateKey
+};
+ const gateway = new braintree.BraintreeGateway(config)
+
+ // to create token
+
+ app.get('/token',  async(req, res) => {
+    try {
+        gateway.clientToken.generate({},(err, response) => {
+            if(err){
+                return res.send({'error' : err})
+            }else{
+                return res.json({'status' : 'success', 'message' : response})
+            }
+        })
+    } catch (err) {
+        return res.json({'error' : err.message})
+    }
+ })
+ 
+ //Transaction
+
+ app.post('/transaction', async(req, res) => {
+   
+    try {
+        const paymentDetail = gateway.tarnsction.sale({amount : req.body.amount,
+            paymentMethodNonce :req.body.paymentMethodNonce,
+            options : 
+            {
+               submitForSettlement :true
+            }
+           } ,(err,response) => {
+            if(response.succes){
+                return res.json({"status" : "success", "message" : response.transaction} )
+            }else{
+                return res.send({err : err})
+            }           
+        })
+        
+    } catch (error) {
+        return res.send({error : error.message})
+        
+    }
+ })
+
+ //server creation
+
+ app.listen(6543, () => {
+    console.log("server started....")
+ })
